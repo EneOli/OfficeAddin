17981e8585129370b22e82cde0e27b4a4627424e
Oliver Enes <oliver@enesnet.de>
2020-12-12 21:57:22 +0100

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require("fs");
+const webpack = require("webpack");
+
+const urlDev="https://localhost:5005/";
+const urlProd="https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+
+module.exports = async (env, options) => {
+  const dev = options.mode === "development";
+  const buildType = dev ? "dev" : "prod";
+  const config = {
+    devtool: "source-map",
+    entry: {
+      polyfill: "@babel/polyfill",
+      commands: "./src/commands/commands.ts"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".html", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: "babel-loader"
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: "ts-loader"
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: "html-loader"
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/,
+          loader: "file-loader",
+          options: {
+            name: '[path][name].[ext]',          
+          }
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: "commands.html",
+        template: "./src/commands/commands.html",
+        chunks: ["polyfill", "commands"]
+      })
+    ],
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },      
+      https: true,
+      port: 5005
+    }
+  };
+
+  return config;
+};
